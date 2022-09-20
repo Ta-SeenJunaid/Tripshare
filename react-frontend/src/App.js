bd1ef703c5c124cc676e7a5edf1bdeb08fbbe78b
Ta-SeenJunaid <taseen.junaid@gmail.com>
2022-09-20 07:50:56 +0200

    User List and user item

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
-import Users from './user/pages/Users'
-import NewPlace from './places/pages/NewPlace'
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
 
 const App = () => {
   return (
