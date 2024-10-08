884fa668ce82d0067f62346a067245cb867900b8
Ta-SeenJunaid <taseen.junaid@gmail.com>
2024-10-08 10:05:36 +0200

    UserItem & UserList

--- tripshare-react-app/src/user/pages/Users.js
+++ tripshare-react-app/src/user/pages/Users.js
@@ -1,7 +1,24 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h1>Users works!!!!!!!!!!!</h1>;
+  const USERS = [
+    {
+      id: "1112ads123",
+      name: "Test User",
+      image:
+        "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      places: 10,
+    },
+    {
+      id: "2112ads123",
+      name: "Test User2",
+      image:
+        "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      places: 1,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
