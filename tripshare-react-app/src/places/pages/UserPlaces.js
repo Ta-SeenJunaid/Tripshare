77306d080626ed753513926bd1ff0d8d0885bd26
Ta-SeenJunaid <taseen.junaid@gmail.com>
2024-12-24 07:46:08 +0100

    Form custom hook & form control

--- tripshare-react-app/src/places/pages/UserPlaces.js
+++ tripshare-react-app/src/places/pages/UserPlaces.js
@@ -19,7 +19,7 @@ const DUMMY_PLACES = [
   },
   {
     id: "id2",
-    title: "Gutkauf",
+    title: "Gutkauf1",
     description: "Good place for grocery",
     imageUrl:
       "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
@@ -31,6 +31,7 @@ const DUMMY_PLACES = [
     creator: "u2",
   },
 ];
+
 const UserPlaces = () => {
   const userId = useParams().userId;
   const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
