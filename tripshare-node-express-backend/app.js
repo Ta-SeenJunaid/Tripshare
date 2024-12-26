27e6c3be4ce262cce4983e32cbef7ebffd977655
Ta-SeenJunaid <taseen.junaid@gmail.com>
2024-12-26 21:39:23 +0100

    Error handling

--- tripshare-node-express-backend/app.js
+++ tripshare-node-express-backend/app.js
@@ -5,6 +5,15 @@ const placesRoutes = require("./routes/places-routes");
 
 const app = express();
 
-app.use(placesRoutes);
+app.use("/api/places", placesRoutes);
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || "An unknoen error occurred!!" });
+});
 
 app.listen(5000);
