3317c67ff48cf446c7f5aa6f779886ac917b2749
Ta-SeenJunaid <taseen.junaid@gmail.com>
2024-12-28 04:34:52 +0100

    places endpoints, routes, controllers

--- tripshare-node-express-backend/controllers/places-controllers.js
+++ tripshare-node-express-backend/controllers/places-controllers.js
@@ -2,7 +2,7 @@ const uuid = require("uuid/v4");
 
 const HttpError = require("../models/http-error");
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: "id1",
     title: "Gutkauf",
@@ -49,10 +49,10 @@ const getPlaceById = (req, res, next) => {
 
 const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
-  const userPlaces = DUMMY_PLACES.filter((p) => {
+  const places = DUMMY_PLACES.filter((p) => {
     return p.creator === userId;
   });
-  if (userPlaces.length === 0) {
+  if (places.length === 0) {
     return next(
       new HttpError(
         "Sorry!!!, Could not find a place for the provided user id.",
@@ -60,7 +60,7 @@ const getPlacesByUserId = (req, res, next) => {
       )
     );
   }
-  res.json({ userPlaces });
+  res.json({ places });
 };
 
 const createPlace = (req, res, next) => {
@@ -79,6 +79,28 @@ const createPlace = (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({ place: updatedPlace });
+};
+
+const deletePlace = (req, res, next) => {
+  const placeId = req.params.pid;
+  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
+  res.status(200).json({ message: "Deleted place." });
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
